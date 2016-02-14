8e5d8ae031b86dde0589e68b1f8c2fcf56a42a2f
Jared Hester <jared.hester@gmail.com>
2016-02-14 02:19:17 -0500

    Project name changed to Forge
    https://github.com/fsprojects/Forge

---
+++ forge.rb
@@ -0,0 +1,17 @@
+class Forge < Formula
+  desc "Forge (F# Project Builder)"
+  homepage "https://github.com/fsprojects/Forge"
+  url "https://github.com/fsprojects/Forge/releases/download/0.4.0/forge.zip"
+  sha256 "a74dc0e50bcf7db5e9380c67c4374ad148b7c107dcbc47425a4a3c36026fcf7a"
+  depends_on "mono"
+  
+  bottle :unneeded
+
+  def install
+    libexec.install Dir["bin/*"]
+    (bin/"forge").write <<-EOS.undent
+      #!/bin/sh
+      mono #{libexec}/forge.exe "$@"
+    EOS
+  end
+end
