23f923d2512e9144572160425439dc5869920805
Jared Hester <jared.hester@gmail.com>
2016-02-14 03:25:52 -0500

    update to latest release

--- forge.rb
+++ forge.rb
@@ -1,7 +1,7 @@
 class Forge < Formula
   desc "Forge (F# Project Builder)"
   homepage "https://github.com/fsprojects/Forge"
-  url "https://github.com/fsprojects/Forge/releases/download/0.4.0/forge.zip"
+  url "https://github.com/fsprojects/Forge/releases/download/0.6.0/forge.zip"
   sha256 "a74dc0e50bcf7db5e9380c67c4374ad148b7c107dcbc47425a4a3c36026fcf7a"
   depends_on "mono"
   
