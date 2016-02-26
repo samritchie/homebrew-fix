f5441b2fd658f08d97dd62917885c4cb761be150
Sam Ritchie <sam@codesplice.com.au>
2016-02-26 16:54:06 +0800

    case-sensitive hash - grr

--- forge.rb
+++ forge.rb
@@ -2,7 +2,7 @@ class Forge < Formula
   desc "Forge (Command line tool for creating F# Projects)"
   homepage "http://fsprojects.github.io/Forge"
   url "https://github.com/fsprojects/Forge/releases/download/0.7.0/forge.zip"
-  sha256 "6E6701FB88A5781E9277EE315902B450FBAC79303AB6EE4FD72F4D83BC897F7A"
+  sha256 "6e6701fb88a5781e9277ee315902b450fbac79303ab6ee4fd72f4d83bc897f7a"
   depends_on "mono"
   
   bottle :unneeded
