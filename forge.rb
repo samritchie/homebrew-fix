26757cdedf43f34fff4a9c566476af6eb7b0a42a
Sam Ritchie <sam@codesplice.com.au>
2016-11-01 21:24:00 +0800

    Updated to 1.0.1

--- forge.rb
+++ forge.rb
@@ -1,23 +1,17 @@
 class Forge < Formula
   desc "Forge (Command line tool for creating F# Projects)"
   homepage "https://fsprojects.github.io/Forge"
-  url "https://github.com/fsprojects/Forge/releases/download/0.7.0/forge.zip"
-  sha256 "6e6701fb88a5781e9277ee315902b450fbac79303ab6ee4fd72f4d83bc897f7a"
+  url "https://github.com/fsprojects/Forge/releases/download/1.0.1/forge.zip"
+  sha256 "b45a9ee51613e1ffbef49c677f9425f9fe85155c303f813251f88403b3c19322"
   depends_on "mono"
 
   bottle :unneeded
-
-  devel do
-    url "https://github.com/fsprojects/Forge/releases/download/1.0.0-alpha012/forge.zip"
-    version "1.0.0-alpha012"
-    sha256 "54fbd49e63e9aa92048a3cbb4bfd6eaeaae214549f47968dfa655eb74d081e46"
-  end
 
   def install
     libexec.install Dir["bin/*"]
     (bin/"forge").write <<-EOS.undent
       #!/bin/sh
-      mono #{libexec}/forge.exe "$@"
+      mono #{libexec}/Forge.exe "$@"
     EOS
   end
 end
