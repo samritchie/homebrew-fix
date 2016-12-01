9c692823fba55acbec3494ea1625e64869f52d0e
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-12-01 17:22:27 +0100

    Update forge.rb

--- forge.rb
+++ forge.rb
@@ -1,8 +1,8 @@
 class Forge < Formula
   desc "Forge (Command line tool for creating F# Projects)"
   homepage "https://forge.run"
-  url "https://github.com/fsharp-editing/Forge/releases/download/1.2.0/forge.zip"
-  sha256 "04a4ec9bbebb79e6296b58ec81bceb4647d09a98fc22faeabb95d464262da6fb"
+  url "https://github.com/fsharp-editing/Forge/releases/download/1.2.1/forge.zip"
+  sha256 "f69695828264461e3f3a2f29828b73323f01e5509ff3b758840280063fa73368"
   depends_on "mono"
 
   bottle :unneeded
